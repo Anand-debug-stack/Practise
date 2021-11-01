98a9df269a6dc33554244287fb53055b95cd85e1
Anand-debug-stack <54739962+Anand-debug-stack@users.noreply.github.com>
2021-11-01 09:56:49 +0530

    test

---
+++ test.java
@@ -0,0 +1,20 @@
+package com.logicinfo.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+
+		System.out.println("first commit the code");
+		
+		
+
+
+	}
+
+}
